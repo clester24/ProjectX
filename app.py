abd1d1e371c568cb02efe278353f0f229b66d915
austinbarrette <71538645+austinbarrette@users.noreply.github.com>
2021-03-28 16:51:25 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import pymongo
+from pymongo import MongoClient
+import json
+from bson import json_util
+
+app = Flask(__name__)
+
+cluster = MongoClient("mongodb+srv://austinbarrette:Mongo123@cluster0.ou3yi.mongodb.net/california_lakes?retryWrites=true&w=majority")
+db = cluster["california_lakes"]
+collection_lake_oroville = db["lake_oroville"]
+collection_lake_shasta = db["lake_shasta"]
+
+#Test
+@app.route('/')
+def test():
+    return "App is working perfectly"
+
+@app.route("/lake_oroville", methods=['GET'])
+def lake_oroville():
+    all_lake_oroville = list(collection_lake_oroville.find({}))
+    return json.dumps(all_lake_oroville, default=json_util.default)
+
+@app.route("/lake_shasta", methods=['GET'])
+def get_all_lake_shasta():
+    all_lake_shasta = list(collection_lake_shasta.find({}))
+    return json.dumps(all_lake_shasta, default=json_util.default)
+
+if __name__ == '__main__':
+    app.run(debug=True)
